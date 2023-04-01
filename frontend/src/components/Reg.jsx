f0810ad4314731778187d3faab29cf2a6c0a40ae
Keval Sutariya <100850842+kevalsutariya710@users.noreply.github.com>
2023-04-01 10:20:43 +0530

    Update Reg.jsx

--- frontend/src/components/Reg.jsx
+++ frontend/src/components/Reg.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 
+
 const Reg = () => {
     const history = useNavigate();
     const [user, setUser] = useState({
