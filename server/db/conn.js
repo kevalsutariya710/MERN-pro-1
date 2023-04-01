7306888db376c1c0711f493bd9dcc57de8cae535
Keval Sutariya <100850842+kevalsutariya710@users.noreply.github.com>
2023-04-01 10:17:56 +0530

    Update conn.js

--- server/db/conn.js
+++ server/db/conn.js
@@ -4,6 +4,6 @@ const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true
 };
-mongoose.connect('mongodb://0.0.0.0:27017/firstdb', options)
+mongoose.connect('/Enter database url/', options)
     .then(() => console.log("connection successful"))
     .catch(err => console.log(err));
