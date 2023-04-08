ef0ef887cd95ff86dc4a4e1aa1466f4d222c337e
Keval Sutariya <100850842+kevalsutariya710@users.noreply.github.com>
2023-04-08 10:42:47 +0530

    Update conn.js

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+
 const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true
+    
 };
 mongoose.connect('/Enter database url/', options)
     .then(() => console.log("connection successful"))
